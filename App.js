6b73593d5670015f57714411ce6e4c05ca873e64
Artur Gaster <gasterartur@googlemail.com>
2024-12-19 10:51:50 +0100

    HW1 done

---
+++ App.js
@@ -0,0 +1,4 @@
+const {logMessage} = require('./logger');
+logMessage ('Первое сообщение в логе');
+logMessage ('Второе сообщение в логе');
+logMessage ('Третье сообщение в логе');
---
+++ logger.js
@@ -0,0 +1,13 @@
+const fs = require ('fs');
+
+function logMessage(message) {
+    const logEntry = `${new Date().toISOString()} -${message}\n`;
+    fs.appendFile('log.txt', logEntry, (err) => {
+        if (err) {
+            console.log('Error writing to log file', err);
+        }
+    });
+            
+}
+
+module.exports = {logMessage};
